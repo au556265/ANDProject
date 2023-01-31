3d3f0cfb1e9efe6d874c23bcb49047c24623ba33
Fatema A <43032320+au556265@users.noreply.github.com>
2023-01-31 16:15:21 +0100

    Implemented filter bookings

--- app/src/main/java/com/github/au556265/myprojectapplication/UI/Bookings/ViewBookingsFragment.java
+++ app/src/main/java/com/github/au556265/myprojectapplication/UI/Bookings/ViewBookingsFragment.java
@@ -11,22 +11,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.au556265.myprojectapplication.Adapter.ViewBookingAdapter;
 import com.github.au556265.myprojectapplication.Models.Booking;
 import com.github.au556265.myprojectapplication.R;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class ViewBookingsFragment extends Fragment {
     private BookingViewModel viewModel;
     RecyclerView recyclerView;
     ViewBookingAdapter adapter;
+    EditText date;
+    Button filterButton;
     View view;
 
     ArrayList<String> adminAccount = new ArrayList<>();
+
+
+
+
+
+    List<Booking> myBookings = new ArrayList<>();
 
 
     @Override
@@ -39,8 +51,17 @@ public class ViewBookingsFragment extends Fragment {
         viewModel.init();
 
         recyclerView = view.findViewById(R.id.rvbookings);
+        date = view.findViewById(R.id.filterDate);
+        filterButton = view.findViewById(R.id.filterDateButton);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.hasFixedSize();
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickFilter();
+            }
+        });
 
         adapter = new ViewBookingAdapter();
         recyclerView.setAdapter(adapter);
@@ -48,6 +69,22 @@ public class ViewBookingsFragment extends Fragment {
         adminAccount.add("admin@hotmail.com");
         getBookings();
         return view;
+    }
+
+    private void onClickFilter() {
+
+        if(date.getText().toString().equals(""))
+            adapter.setBookingItems(myBookings);
+        else
+        {
+            ArrayList<Booking> filteredBookings = new ArrayList<>();
+
+            for (int i = 0; i < myBookings.size(); i++) {
+                if(Objects.equals(myBookings.get(i).getBookingDate(), date.getText().toString()))
+                    filteredBookings.add(myBookings.get(i));
+            }
+            adapter.setBookingItems(filteredBookings);
+        }
     }
 
     private void getBookings() {
@@ -62,11 +99,12 @@ public class ViewBookingsFragment extends Fragment {
                     }
                 }
                 if(!isAdmin){
-                    List<Booking> myBookings = viewModel.getOnlyMyBookings();
+                    myBookings = viewModel.getOnlyMyBookings();
                     adapter.setBookingItems(myBookings);
                 }
                 else {
-                    adapter.setBookingItems(bookings);
+                    myBookings = bookings;
+                    adapter.setBookingItems(myBookings);
                 }
 
             }
